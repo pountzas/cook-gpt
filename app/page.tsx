69d7932013d5f0fee3dc83fe315306ce9e2f13c6
pountzas <nikos@pountzas.gr>
2023-03-03 01:48:14 +0200

    add basic structure to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
+import { SunIcon } from "@heroicons/react/24/outline";
+
 function HomePage() {
-  return <div className="text-red-500">HomePage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+      <h1 className="mb-20 text-5xl font-bold">cookGPT</h1>
+
+      <div>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <SunIcon className="w-8 h-8" />
+            <h2>Examples</h2>
+          </div>
+
+          <div className="grid grid-cols-3 gap-2 text-center">
+            <p className="exampleText">Lemon Pasta</p>
+            <p className="exampleText">Mousaka</p>
+            <p className="exampleText">Greek Lamb Kleftiko</p>
+            <p className="exampleText">Moroccan Chicken</p>
+            <p className="exampleText">Chicken Tikka Masala</p>
+            <p className="exampleText">Chicken Korma</p>
+            <p className="exampleText">Baked Cod with Lemon and Garlic</p>
+            <p className="exampleText">Watermelon Salad</p>
+            <p className="exampleText">Greek Salad</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
