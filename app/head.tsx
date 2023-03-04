de43136d8ad7718c45743702443f42c40986caf6
pountzas <nikos@pountzas.gr>
2023-03-05 01:05:05 +0200

    remove head file old metadata

--- app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>cookGPT</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  );
-}
