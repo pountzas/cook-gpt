a9f33a862dbd4d4a325e88e5b33980081bb83974
pountzas <nikos@pountzas.gr>
2023-03-05 01:45:27 +0200

    add styles to sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,9 @@ export default async function RootLayout({
             <Login />
           ) : (
             <div className="flex">
-              <SideBar />
+              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                <SideBar />
+              </div>
               <div className="bg-[#343541] flex-1">{children}</div>
             </div>
           )}
