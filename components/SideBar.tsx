cab8c02bbd4c62c0c5d98d3ed23a219eceabb1de
pountzas <nikos@pountzas.gr>
2023-03-05 02:27:02 +0200

    add user icon w/ signout onclick

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
 import NewRecipe from "./NewRecipe";
 
 function SideBar() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col h-screen p-2 ">
       <div className="flex-1">
-        <NewRecipe />
-        <div>map recipe selection</div>
+        <div>
+          <NewRecipe />
+          <div>map recipe selection</div>
+        </div>
       </div>
+
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="Profile pic"
+          className="w-12 h-12 mx-auto mb-2 rounded-full cursor-pointer"
+        />
+      )}
     </div>
   );
 }
