77eeca50f51d5e9b84afed0ac921b9a9fe3b5e1e
pountzas <nikos@pountzas.gr>
2023-03-06 05:29:02 +0200

    externalize premade recipes from main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { SunIcon } from "@heroicons/react/24/outline";
 import CookGPTLogo from "../public/assets/svg/CookGPTLogo";
+import { premadeRecipies } from "../lib/premadeRecipies";
 
 function HomePage() {
   return (
@@ -15,15 +16,11 @@ function HomePage() {
           </div>
 
           <div className="grid grid-cols-3 gap-2 text-center ">
-            <p className="exampleText">Lemon Pasta</p>
-            <p className="exampleText">Mousaka</p>
-            <p className="exampleText">Greek Lamb Kleftiko</p>
-            <p className="exampleText">Moroccan Chicken</p>
-            <p className="exampleText">Chicken Tikka Masala</p>
-            <p className="exampleText">Chicken Korma</p>
-            <p className="exampleText">Baked Cod with Lemon and Garlic</p>
-            <p className="exampleText">Watermelon Salad</p>
-            <p className="exampleText">Greek Salad</p>
+            {premadeRecipies.map((recipe) => (
+              <p key={recipe.id} className="exampleText">
+                {recipe.name}
+              </p>
+            ))}
           </div>
         </div>
       </div>
---
+++ lib/premadeRecipies.tsx
@@ -0,0 +1,174 @@
+export const premadeRecipies = [
+  {
+    id: 1,
+    name: "Lemon Pasta",
+    ingredients: [
+      "pasta",
+      "lemon",
+      "garlic",
+      "olive oil",
+      "salt",
+      "pepper",
+      "parsley"
+    ],
+    instructions: [""]
+  },
+  {
+    id: 2,
+    name: "Mousaka",
+    ingredients: [
+      "eggplant",
+      "onion",
+      "garlic",
+      "olive oil",
+      "salt",
+      "pepper",
+      "ground beef",
+      "tomato paste",
+      "tomato sauce",
+      "bay leaf",
+      "cinnamon",
+      "nutmeg",
+      "allspice",
+      "milk",
+      "parmesan cheese",
+      "salt",
+      "pepper"
+    ],
+    instructions: [""]
+  },
+  {
+    id: 3,
+    name: "Greek Lamb Kleftiko",
+    ingredients: [
+      "lamb shoulder",
+      "olive oil",
+      "salt",
+      "pepper",
+      "garlic",
+      "onion",
+      "tomato paste",
+      "tomato sauce",
+      "bay leaf",
+      "cinnamon",
+      "nutmeg",
+      "allspice",
+      "parsley",
+      "lemon"
+    ],
+    instructions: [""]
+  },
+  {
+    id: 4,
+    name: "Moroccan Chicken",
+    ingredients: [
+      "chicken",
+      "olive oil",
+      "salt",
+      "pepper",
+      "garlic",
+      "onion",
+      "tomato paste",
+      "tomato sauce",
+      "bay leaf",
+      "cinnamon",
+      "nutmeg",
+      "allspice",
+      "parsley",
+      "lemon"
+    ],
+    instructions: [""]
+  },
+  {
+    id: 5,
+    name: "Chicken Tikka Masala",
+    ingredients: [
+      "chicken",
+      "olive oil",
+      "salt",
+      "pepper",
+      "garlic",
+      "onion",
+      "tomato paste",
+      "tomato sauce",
+      "bay leaf",
+      "cinnamon",
+      "nutmeg",
+      "allspice",
+      "parsley",
+      "lemon"
+    ],
+    instructions: [""]
+  },
+  {
+    id: 6,
+    name: "Chicken Korma",
+    ingredients: [
+      "chicken",
+      "olive oil",
+      "salt",
+      "pepper",
+      "garlic",
+      "onion",
+      "tomato paste",
+      "tomato sauce",
+      "bay leaf",
+      "cinnamon",
+      "nutmeg",
+      "allspice",
+      "parsley",
+      "lemon"
+    ],
+    instructions: [""]
+  },
+  {
+    id: 7,
+    name: "Baked Cod with Lemon and Garlic",
+    ingredients: [
+      "cod",
+      "olive oil",
+      "salt",
+      "pepper",
+      "garlic",
+      "onion",
+      "tomato paste",
+      "tomato sauce",
+      "bay leaf",
+      "cinnamon",
+      "nutmeg",
+      "allspice",
+      "parsley",
+      "lemon"
+    ],
+    instructions: [""]
+  },
+  {
+    id: 8,
+    name: "Watermelon Salad",
+    ingredients: [
+      "watermelon",
+      "feta cheese",
+      "mint",
+      "olive oil",
+      "salt",
+      "pepper",
+      "lemon"
+    ],
+    instructions: [""]
+  },
+  {
+    id: 9,
+    name: "Greek Salad",
+    ingredients: [
+      "tomato",
+      "cucumber",
+      "onion",
+      "feta cheese",
+      "olive oil",
+      "salt",
+      "pepper",
+      "lemon"
+    ],
+    instructions: [""]
+  }
+];
