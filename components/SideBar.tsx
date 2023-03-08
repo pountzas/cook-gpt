24b0bfb0585c05ab1c16c9c344a34592ca7aa371
pountzas <nikos@pountzas.gr>
2023-03-08 20:55:58 +0200

    descending map from firestore

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -4,14 +4,18 @@ import { useSession, signOut } from "next-auth/react";
 import NewRecipe from "./NewRecipe";
 import RecipeItem from "./RecipeItem";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { collection } from "firebase/firestore";
+import { collection, orderBy, query } from "firebase/firestore";
 import { db } from "../firebase";
 
 function SideBar() {
   const { data: session } = useSession();
 
   const [recipes, loading, error] = useCollection(
-    session && collection(db, "users", session.user?.email!, "recipes")
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "recipes"),
+        orderBy("createdAt", "desc")
+      )
   );
 
   return (
