980adc91f302983ce7ec3d5bcdda5ad36ce272e3
pountzas <nikos@pountzas.gr>
2023-03-09 23:37:41 +0200

    add recoilRoot on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
-// "use client";
-
 import "../styles/globals.css";
 
-import Login from "../components/Login";
-import SessionProvider from "../components/SessionProvider";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../pages/api/auth/[...nextauth]";
 import type { Metadata } from "next";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import { ReactNode } from "react";
+
+import RecoilRoot from "../components/RecoilRoot";
+import SessionProvider from "../components/SessionProvider";
+import Login from "../components/Login";
 import SideBar from "../components/SideBar";
 
 export const metadata: Metadata = {
   title: "cookGPT",
   description:
     "Search for recipies using a hashtag or a post url from social media",
-  keywords: ["recipes", "food", "hashtag", "recipie post url", "cookGPT"],
+  keywords: ["recipes", "food", "hashtag", "recipe post url", "cookGPT"],
   creator: "Nikos Pountzas"
 };
 
 export default async function RootLayout({
   children
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   const session = await getServerSession(authOptions);
 
@@ -29,16 +30,18 @@ export default async function RootLayout({
       <head />
       <body>
         <SessionProvider session={session}>
-          {!session ? (
-            <Login />
-          ) : (
-            <div className="flex">
-              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
-                <SideBar />
+          <RecoilRoot>
+            {!session ? (
+              <Login />
+            ) : (
+              <div className="flex">
+                <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                  <SideBar />
+                </div>
+                <div className="bg-[#343541] flex-1">{children}</div>
               </div>
-              <div className="bg-[#343541] flex-1">{children}</div>
-            </div>
-          )}
+            )}
+          </RecoilRoot>
         </SessionProvider>
       </body>
     </html>
