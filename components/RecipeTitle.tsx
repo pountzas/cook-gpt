a21d4c2c2e5e637766ea32c9d1d21038862aeb38
pountzas <nikos@pountzas.gr>
2023-03-09 23:39:30 +0200

    create recipeTitle w/ animation logic

---
+++ components/RecipeTitle.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { mainTitleAtom } from "../atoms/dataAtom";
+import { motion } from "framer-motion";
+
+type Props = {
+  id: string;
+};
+
+function RecipeTitle({ id }: Props) {
+  const [mainTitle, setMainTitle] = useRecoilState(mainTitleAtom);
+  const [headerTitleArray, setHeaderTitleArray] = useState([]);
+  const [headerTitle, setHeaderTitle] = useState("");
+
+  useEffect(() => {
+    setHeaderTitle(mainTitle);
+    setHeaderTitleArray(Array.from(headerTitle));
+  }, []);
+
+  useEffect(() => {
+    setHeaderTitleArray(Array.from(headerTitle));
+  }, [headerTitle]);
+
+  useEffect(() => {
+    setHeaderTitle("");
+
+    setTimeout(() => {
+      setHeaderTitle(mainTitle);
+    }, 100);
+  }, [mainTitle]);
+
+  return (
+    <div>
+      {headerTitleArray?.map((letter, index) => {
+        return (
+          <motion.span
+            initial={{ opacity: 0, y: -100, z: -500 }}
+            animate={{ opacity: 1, y: 0, z: 0 }}
+            transition={{ delay: index * 0.1 }}
+            key={index}
+            className="inline-block"
+          >
+            {letter === " " ? "\u00A0" : letter}
+          </motion.span>
+        );
+      })}
+    </div>
+  );
+}
+
+export default RecipeTitle;
