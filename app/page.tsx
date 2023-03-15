4b8913610c143de448861198303b4c2c7d729003
pountzas <nikos@pountzas.gr>
2023-03-15 19:25:04 +0200

    create PremadeRecipeItem & add to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { SunIcon } from "@heroicons/react/24/outline";
-import CookGPTLogo from "../public/assets/svg/CookGPTLogo";
 import { premadeRecipies } from "../lib/premadeRecipies";
+import Logo from "../components/Logo";
+import PremadeRecipeItem from "../components/PremadeRecipeItem";
 
 function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
-      <CookGPTLogo size={250} />
+      <Logo />
       <h1 className="my-20 text-5xl font-bold">cookGPT</h1>
 
       <div>
@@ -17,9 +18,12 @@ function HomePage() {
 
           <div className="grid grid-cols-3 gap-2 text-center ">
             {premadeRecipies.map((recipe) => (
-              <p key={recipe.id} className="exampleText">
-                {recipe.name}
-              </p>
+              <PremadeRecipeItem
+                key={recipe.id}
+                name={recipe.name}
+                ingredients={recipe.ingredients}
+                instructions={recipe.instructions}
+              />
             ))}
           </div>
         </div>
---
+++ components/PremadeRecipeItem.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useRecoilState } from "recoil";
+import {
+  mainTitleAtom,
+  premadeIngredientsAtom,
+  premadeInstructionsAtom
+} from "../atoms/dataAtom";
+
+type Props = {
+  key: number;
+  name: string;
+  // aray of strings
+
+  instructions: never[];
+  ingredients: never[];
+};
+
+function PremadeRecipeItem({ key, name, instructions, ingredients }: Props) {
+  const [mainTitle, setMainTitle] = useRecoilState(mainTitleAtom);
+  const [premadeIngredients, setPremadeIngredients] = useRecoilState(
+    premadeIngredientsAtom
+  );
+  const [premadeInstructions, setPremadeInstructions] = useRecoilState(
+    premadeInstructionsAtom
+  );
+
+  const router = useRouter();
+
+  const handlePremadePage = () => {
+    setMainTitle(name);
+    setPremadeIngredients(ingredients);
+    setPremadeInstructions(instructions);
+    router.push(`/recipes/${key}`);
+  };
+
+  return (
+    <h2 onClick={handlePremadePage} className="exampleText">
+      {name}
+    </h2>
+  );
+}
+
+export default PremadeRecipeItem;
