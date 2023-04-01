64cb03b27fcbe2f1d4d91cde90d1fb5cac4c1976
pountzas <nikos@pountzas.gr>
2023-04-01 17:26:46 +0300

    set env on next.config

--- next.config.js
+++ next.config.js
@@ -5,5 +5,17 @@ module.exports = {
     appDir: true,
 
   },
+  env: {
+    SCRAPER_API: process.env.SCRAPER_API,
+    CHATGPT_API_KEY: process.env.CHATGPT_API_KEY,
+    GOOGLE_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    MESSAGE_PROMT: process.env.MESSAGE_PROMT,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+  images: {
+    domains: ['assets', 'localhost']
+  },
 
 }
