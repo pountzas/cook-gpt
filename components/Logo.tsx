474465430d12d86b509ccd62297f6b73ce03b4fc
pountzas <nikos@pountzas.gr>
2023-09-06 22:07:02 +0300

    adjust logo animation damping

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -8,7 +8,7 @@ function Logo() {
     <motion.div
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 1, type: "spring", stiffness: 400 }}
+      transition={{ delay: 1, type: "spring", damping: 10, stiffness: 400 }}
       className="motion"
     >
       <CookGPTLogo size={250} />
