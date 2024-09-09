14f70e1a7177befcf415673c48f1e32291282a28
pountzas <nikos@pountzas.gr>
2024-09-09 17:31:15 +0300

    correct google env matches

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
   env: {
     SCRAPER_API: process.env.SCRAPER_API,
     CHATGPT_API_KEY: process.env.CHATGPT_API_KEY,
-    GOOGLE_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
     MESSAGE_PROMT: process.env.MESSAGE_PROMT,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
