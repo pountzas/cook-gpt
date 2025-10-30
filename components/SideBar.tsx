f758a6e383347efcf07d2f8fcc10b09cdfeeda2b
pountzas <nikos@pountzas.gr>
2025-10-30 17:10:47 +0200

    Update SideBar component to improve scroll behavior with max height and overflow settings

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -21,7 +21,7 @@ function SideBar() {
   return (
     <div className="flex flex-col h-screen p-2 ">
       <div className="flex-1">
-        <div>
+        <div className="max-h-[calc(100vh-100px)] overflow-y-auto scrollbar-track-gray-500 scrollbar-thin scrollbar-thumb-[#202123]/80">
           <NewRecipe />
 
           {recipes?.docs.map((recipe) => (
