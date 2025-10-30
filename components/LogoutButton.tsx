2569edd2b81390d447555dbcb95f87376d0ab961
pountzas <nikos@pountzas.gr>
2025-10-30 19:22:21 +0200

    Refactor SideBar component to utilize RecipeList and add "use client" directive in LogoutButton
    - Updated SideBar to replace inline recipe rendering with RecipeList for improved organization.
    - Added "use client" directive in LogoutButton for compatibility with client-side rendering.

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Activity } from "react";
 import { useSession, signOut } from "next-auth/react";
 
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,36 +1,15 @@
-
-import { useSession } from "next-auth/react";
 import NewRecipe from "./NewRecipe";
-import RecipeItem from "./RecipeItem";
-import { useCollection } from "react-firebase-hooks/firestore";
-import { collection, orderBy, query } from "firebase/firestore";
-import { db } from "../firebase";
 import LogoutButton from "./LogoutButton";
+import RecipeList from "./RecipeList";
 
 function SideBar() {
-  const { data: session } = useSession();
-
-  const [recipes, loading, error] = useCollection(
-    session &&
-      query(
-        collection(db, "users", session.user?.email!, "recipes"),
-        orderBy("createdAt", "desc")
-      )
-  );
-
   return (
     <div className="flex flex-col h-screen p-2 ">
       <div className="flex-1">
         <div className="max-h-[calc(100vh-100px)] overflow-y-auto scrollbar-track-gray-500 scrollbar-thin scrollbar-thumb-[#202123]/80">
           <NewRecipe />
 
-          {recipes?.docs.map((recipe) => (
-            <RecipeItem
-              key={recipe.id}
-              id={recipe.id}
-              title={recipe.data().title}
-            />
-          ))}
+          <RecipeList />
         </div>
       </div>
 
